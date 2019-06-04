64c8761c970a974b423b78dadd5d4d9b680ea22b
LEO D PEN <33611404+LEODPEN@users.noreply.github.com>
2019-06-04 20:36:51 +0800

    add corsconfig;filter to ensure

--- src/main/java/com/daxueshi/sqlwork/config/GlobalCorConfig.java
+++ src/main/java/com/daxueshi/sqlwork/config/GlobalCorConfig.java
@@ -21,7 +21,7 @@ public class GlobalCorConfig {
         //放行哪些原始域(头部信息)
         config.addAllowedHeader("*");
         //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
-        config.addExposedHeader("*");
+//        config.addExposedHeader("*");
 
         //2.添加映射路径
         UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
